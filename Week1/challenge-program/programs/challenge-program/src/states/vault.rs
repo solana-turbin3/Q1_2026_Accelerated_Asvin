fb495f6f6f5041ea51b660773db745e82cd00f25
kiralightyagami <crucisgamer98@gmail.com>
2026-02-09 10:54:17 +0530

    forgot to save

--- Week1/challenge-program/programs/challenge-program/src/states/vault.rs
+++ Week1/challenge-program/programs/challenge-program/src/states/vault.rs
@@ -3,7 +3,9 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct VaultState {
-    pub amount: u64,
-    pub vault_bump: u8,
-    pub state_bump: u8,
+    pub mint: Pubkey,           // The token mint
+    pub authority: Pubkey,       // Who can manage the vault
+    pub total_deposited: u64,    // Total amount in vault
+    pub vault_bump: u8,          // Bump for the vault account
+    pub state_bump: u8,          // Bump for the state account
 }
--- Week1/challenge-program/programs/challenge-program/src/states/whitelist.rs
+++ Week1/challenge-program/programs/challenge-program/src/states/whitelist.rs
@@ -4,6 +4,7 @@ use anchor_lang::prelude::*;
 #[derive(InitSpace)]
 pub struct Whitelist {
     pub address: Pubkey,
+    pub amount: u64, 
     pub is_whitelisted: bool,
     pub bump: u8,
 }
